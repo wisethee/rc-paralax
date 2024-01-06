32689da4e76ed33c489f4048fd3f1b14b6d0c4ae
Marius P <marius.paduraru@gmail.com>
2024-01-06 16:43:05 +0000

    Add @react-spring/parallax dependency and update index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,372 @@
-function App() {
-  return <>App.</>;
-}
+import { useEffect } from "react";
 
-export default App;
+export default function App() {
+  useEffect(() => {
+    const handleScroll = (e) => {
+      e.preventDefault();
+      window.scrollBy({
+        top: e.deltaY * 1,
+        behavior: "smooth",
+      });
+    };
+
+    window.addEventListener("wheel", handleScroll, { passive: false });
+
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "600px",
+        height: "100%",
+        margin: "12rem auto",
+      }}
+    >
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat
+        ratione temporibus tempora, minus, doloremque sequi explicabo omnis
+        veritatis culpa laborum. Quas quasi minima quae ducimus laborum quo
+        asperiores sit.
+      </p>
+    </div>
+  );
+}
